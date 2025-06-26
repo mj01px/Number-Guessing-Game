809e025aaa328045abba1d86e313393e95ee6eb0
mauro junior <125211102+mj01px@users.noreply.github.com>
2025-06-25 23:57:56 -0300

    feat: add voice control with permissions on index page

--- src/js/main.js
+++ src/js/main.js
@@ -1,55 +1,20 @@
-let randomNumber = Math.floor(Math.random() * 10) + 1;
-let attempts = 1;
-
+// Main JavaScript file for the Find Out the Number game
 function displayGameInfo(tag, text) {
     let camp = document.querySelector(tag);
     camp.innerHTML = text;
 }
 
-displayGameInfo('title', 'Find out the Number');
+// Display game information
 displayGameInfo('#question-text', 'Find out the Number');
 displayGameInfo('#question-subtext', 'I thought of a number between 0 and 10. Can you guess it?');
 
-function sendButton() {
-    // Get the input value
-    let input = document.querySelector('input').value;
-
-    // Check if the input is empty
-    if (input === '') {
-        showAlert('Please enter a number!');
-        return;
-    }
-
-    input = Number(input);
-
-    // Check if the input is a valid number
-    if (input < 0 || input > 10) {
-        showAlert('Please enter a number between 0 and 10!');
-        return;
-    }
-
-    // Compare the input with the random number
-    if (input < randomNumber) {
-        showAlert(`The secret number is greater than ${input}. Try again!`);
-        attempts++;
-        return;
-    } else if (input > randomNumber) {
-        showAlert(`The secret number is less than ${input}. Try again!`);
-        attempts++;
-        return;
-    }
-
-    // If the input matches the random number, display a success message
-    let attemptsText = attempts > 1 ? "attempts" : "attempt";
-    showAlert(`Congratulations! You guessed the secret number in ${attempts} ${attemptsText}.`);
+// Function to speak the initial game instructions
+function speakText() {
+    responsiveVoice.speak("Find out the Number. I thought of a number between 0 and 10. Can you guess it");
 }
+speakText()
 
-// Reset the game
-function ngButton() {
-    randomNumber = Math.floor(Math.random() * 10) + 1;
-    attempts = 1;
-    showAlert('New game started! Try to guess the new number.');
-    //clear the input field
-    document.querySelector('input').value = '';
-
+// Redirect to the number guessing game page
+function startGame() {
+    window.location.href = "http://localhost:63342/guessing-number-game/find-out-the-number/src/web/number-guess.html";
 }
---
+++ src/js/number-guess.js
@@ -0,0 +1,64 @@
+let randomNumber = 4
+let attempts = 1;
+
+// Function to show an alert message
+function displayGameInfo(tag, text) {
+    let camp = document.querySelector(tag);
+    camp.innerHTML = text;
+}
+
+displayGameInfo('#question-text', 'Find out the Number');
+displayGameInfo('#question-subtext', 'I thought of a number between 0 and 10. Can you guess it?');
+
+function sendButton() {
+    // Get the input value
+    let input = document.querySelector('input').value;
+
+    // Check if the input is empty
+    if (input === '') {
+        showAlert('Please enter a number!');
+        return;
+    }
+
+    input = Number(input);
+
+    // Check if the input is a valid number
+    if (input < 0 || input > 10) {
+        showAlert('Please enter a number between 0 and 10!');
+        return;
+    }
+
+    // Compare the input with the random number
+    if (input < randomNumber) {
+        showAlert(`The secret number is greater than ${input}. Try again!`);
+        attempts++;
+        return;
+    } else if (input > randomNumber) {
+        showAlert(`The secret number is less than ${input}. Try again!`);
+        attempts++;
+        return;
+    }
+
+    // If the input matches the random number, display a success message
+    let attemptsText = attempts > 1 ? "attempts" : "attempt";
+    showAlert(`Congratulations! You guessed the secret number in ${attempts} ${attemptsText}.`);
+}
+
+// Reset the game
+function ngButton() {
+    randomNumber = Math.floor(Math.random() * 10) + 1;
+    attempts = 1;
+    showAlert('New game started! Try to guess the new number.');
+    //clear the input field
+    document.querySelector('input').value = '';
+
+}
+
+// Reload to index page
+function ReloadToIndex() {
+    if (performance.getEntriesByType("navigation")[0].type == "reload") {
+        window.location.href = "http://localhost:63342/guessing-number-game/find-out-the-number/src/index.html";
+    }
+}
+
+ReloadToIndex();
